8231dd01008d24183291dce9e09e9f6b4bf9017c
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-05-04 20:15:30 -0700

    Update SQL4solution.sql

--- SQL4solution.sql
+++ SQL4solution.sql
@@ -25,3 +25,19 @@ UNION
 SELECT 'Junior' AS experience, COUNT(employee_id) AS accepted_candidates FROM CTE
 Where experience = 'Junior' AND rsum <= (SELECT remaining FROM ACTE);
 
+#1841. League Statistics
+# Write your MySQL query statement below
+WITH CTE AS (
+    SELECT home_team_id AS r1, away_team_id AS r2, home_team_goals AS g1, away_team_goals AS g2
+    FROM Matches
+    UNION ALL
+    SELECT away_team_id AS r1, home_team_id AS r2, away_team_goals AS g1, home_team_goals AS g2
+    FROM Matches)
+
+SELECT t.team_name, count(r1) AS 'matches_played', SUM(
+    Case
+       WHEN c.g1>c.g2 Then 3
+       WHEN c.g1 = c.g2 Then 1
+       ELSE 0
+       END) AS 'points', SUM(c.g1) AS 'goal_for', SUM(c.g2) AS 'goal_against' , SUM(c.g1) - SUM(c.g2) AS 'goal_diff' FROM Teams t JOIN CTE c ON t.team_id = c.r1 GROUP BY c.r1 Order by points DESC, goal_diff DESC, t.team_name;
+
